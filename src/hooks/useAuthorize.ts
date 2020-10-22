b9611167cf20f8fb7d1c2fe3f78c233a87bcca8e
Renato Rodrigues <rrodriguesicc@gmail.com>
2020-10-22 16:54:30 -0300

    Adding hooks

---
+++ src/hooks/useAuthorize.ts
@@ -0,0 +1,26 @@
+import { AuthorizationStrategy } from '../types';
+import { usePermissions } from './usePermissions';
+
+export const useAuthorize = (
+  requirements: Array<string>,
+  authorizationStrategy?: AuthorizationStrategy<any, any>,
+): boolean => {
+  const {
+    permissions,
+    authorizationStrategy: providerStrategy,
+  } = usePermissions();
+
+  const defaultStrategy = () => {
+    return !!permissions.find(state => requirements.includes(state));
+  };
+
+  if (authorizationStrategy) {
+    return authorizationStrategy(permissions, requirements);
+  }
+
+  if (providerStrategy) {
+    return providerStrategy(permissions, requirements);
+  }
+
+  return defaultStrategy();
+};
