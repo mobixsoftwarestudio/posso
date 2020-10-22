35419cf190fd40a058e0c82fd32806cf28c24b46
Renato Rodrigues <rrodriguesicc@gmail.com>
2020-10-22 16:55:31 -0300

    export all

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,5 @@
-export {
-CheckPermission
-} from './components/CheckPermission';
+export { CheckPermission } from './components/CheckPermission';
+export { PossoRoutes } from './router/PossoRoutes';
+export { PossoProvider } from './provider';
+export { useAuthorize } from './hooks/useAuthorize';
+export { usePermissions } from './hooks/usePermissions';
