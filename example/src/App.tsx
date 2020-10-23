edfd1251dfe16c0c6bdcc932534519a0717a49e9
Renato Rodrigues <rrodriguesicc@gmail.com>
2020-10-23 11:42:12 -0300

    Improving examples

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,19 +1,32 @@
 import React from 'react'
 
-import { BrowserRouter, Switch } from 'react-router-dom';
-import { PossoProvider, PossoRoutes } from 'posso';
+import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
+import { PossoProvider, PossoRoute } from 'posso';
 import Home from './screens/Home';
 import About from './screens/About';
+import Contact from './screens/Contact';
+import Login from './screens/Login';
 
 const App = () => {
-  const permissionsOfApp = ['page/about', 'page/home'];
+  const permissionsOfApp = ['page/home'];
+  const isAuthenticated = sessionStorage.getItem('exampleToken');
+
+  const NotAllowedComponent = () => {
+    return (
+      <div>
+        <h2>Not allowed page.</h2>
+      </div>
+    )
+  }
 
   return (
-    <PossoProvider permissions={permissionsOfApp}>
+    <PossoProvider permissions={permissionsOfApp} isAuthenticated={isAuthenticated !== null} notAuthenticatedRedirect={<Redirect to="/" />}>
       <BrowserRouter>
         <Switch>
-          <PossoRoutes exact path={'/home'} component={Home} permissions={['page/home']} />
-          <PossoRoutes exact path={'/about'} component={About} permissions={['page/about']} />
+          <Route exact path={'/'} component={Login} />
+          <PossoRoute exact path={'/home'} component={Home} permissions={['page/home']} />
+          <PossoRoute exact path={'/about'} component={About} permissions={['page/about']} notAllowedComponent={<NotAllowedComponent />} />
+          <PossoRoute exact path={'/contact'} component={Contact} permissions={['page/contact']} />
         </Switch>
       </BrowserRouter>
     </PossoProvider>
---
+++ example/src/screens/Login.tsx
@@ -0,0 +1,37 @@
+import React, { FormEvent } from 'react';
+
+const Login: React.FC = () => {
+  const [email, setEmail] = React.useState('');
+  const [password, setPassword] = React.useState('');
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    sessionStorage.setItem('exampleToken', 'token');
+    window.location.replace('/home');
+  }
+
+  return (
+    <div>
+      <form className='react-form' onSubmit={handleSubmit}>
+        <h1>Hi folks!</h1>
+
+        <fieldset className='form-group'>
+          <label htmlFor="formEmail">Email:</label>
+
+          <input id='formEmail' className='form-input' name='email' type='email' required onChange={e => setEmail(e.target.value)} value={email} />
+        </fieldset>
+        <fieldset className='form-group'>
+          <label htmlFor="formPassword">Password:</label>
+
+          <input id='formPassword' className='form-input' name='password' type='password' required onChange={e => setPassword(e.target.value)} value={password} />
+        </fieldset>
+
+        <div className='form-group'>
+          <input id='formButton' className='btn' type='submit' placeholder='Send message' />
+        </div>
+      </form>
+    </div>
+  );
+}
+
+export default Login;
