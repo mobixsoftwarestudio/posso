c8ee4200da3dce820b64b932c833eb2bcf9dc042
Renato Rodrigues <rrodriguesicc@gmail.com>
2020-10-23 11:44:53 -0300

    Updates

---
+++ precache-manifest.b7c3e23b6410e48fd58e6600948d1a61.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba06cd0c8251f4bf30ac3a1b11fcbcfb",
+    "url": "./index.html"
+  },
+  {
+    "revision": "af0e8f66e23516ebbb03",
+    "url": "./static/css/main.f2fa5379.chunk.css"
+  },
+  {
+    "revision": "c48eec87fd7f2f3064b0",
+    "url": "./static/js/2.2ba30885.chunk.js"
+  },
+  {
+    "revision": "3c73c585782ac05880c0f89bcfdbba5a",
+    "url": "./static/js/2.2ba30885.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af0e8f66e23516ebbb03",
+    "url": "./static/js/main.1b13e40d.chunk.js"
+  },
+  {
+    "revision": "891b93a30cce6c6bb1ba",
+    "url": "./static/js/runtime-main.123e97d8.js"
+  }
+]);
